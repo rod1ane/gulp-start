55631566252e676328809710cd7566c1aaf138cd
rodiane <radion.yu@gmail.com>
2021-01-19 10:42:35 +0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 let fileswatch = 'html,htm,json'
 
-const {src, dest, series, watch, parallel}   = require('gulp')
-const sass          = require('gulp-sass')
-const sync          = require('browser-sync').create()
-const del           = require('del')
-const autoprefixer  = require('gulp-autoprefixer')
-const rename        = require('gulp-rename')
-const notify        = require('gulp-notify')
-const concat        = require('gulp-concat')
+const {
+  src,
+  dest,
+  series,
+  watch,
+  parallel
+} = require('gulp')
+const sass = require('gulp-sass')
+const sync = require('browser-sync').create()
+const del = require('del')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const notify = require('gulp-notify')
+const concat = require('gulp-concat')
 
 
 function commonJs() {
@@ -17,79 +23,95 @@ function commonJs() {
 }
 
 function scripts() {
-	return src([
-		'app/libs/jquery/dist/jquery.min.js',
-		// 'app/libs/owl-carousel/owl.carousel.js',
-		// 'app/libs/jquery.formstyler.js',
-		'app/libs/jquery.matchHeight-min.js',
-		// 'app/libs/ofi.min.js',
-		// 'app/libs/swiper.js',
-		// 'app/libs/jquery.fancybox.min.js',
-		// 'app/libs/jquery.responsiveTabs.min.js',
-		// 'app/libs/jquery.doubletaptogo.js',
-		// 'app/libs/jquery.mmenu.all.min.js',
-		// 'app/libs/rellax.min.js',
-		// 'app/libs/handorgel.min.js',
-		'app/js/common.min.js', // Всегда в конце
-		])
-	.pipe(concat('scripts.min.js'))
-	.pipe(dest('app/js'))
-	.pipe(sync.stream());
+  return src([
+      'app/libs/jquery/dist/jquery.min.js',
+      // 'app/libs/owl-carousel/owl.carousel.js',
+      // 'app/libs/jquery.formstyler.js',
+      'app/libs/jquery.matchHeight-min.js',
+      // 'app/libs/ofi.min.js',
+      // 'app/libs/swiper.js',
+      // 'app/libs/jquery.fancybox.min.js',
+      // 'app/libs/jquery.responsiveTabs.min.js',
+      // 'app/libs/jquery.doubletaptogo.js',
+      // 'app/libs/jquery.mmenu.all.min.js',
+      // 'app/libs/rellax.min.js',
+      // 'app/libs/handorgel.min.js',
+      'app/js/common.min.js', // Всегда в конце
+    ])
+    .pipe(concat('scripts.min.js'))
+    .pipe(dest('app/js'))
+    .pipe(sync.stream());
 }
 
 function browsersync() {
-	sync.init({
-		server: { baseDir: 'app/' },
-		notify: false,
-		online: true,
-		open: false
-	})
+  sync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    notify: false,
+    online: true,
+    open: false
+  })
 }
 
 function styles() {
-	return src('app/sass/**/*.sass')
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(rename({suffix: '.min', prefix : ''}))
-	.pipe(dest('app/css'))
-	.pipe(sync.stream())
+  return src('app/sass/**/*.sass')
+    .pipe(sass({
+      outputStyle: 'expand'
+    }).on("error", notify.onError()))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(rename({
+      suffix: '.min',
+      prefix: ''
+    }))
+    .pipe(dest('app/css'))
+    .pipe(sync.stream())
 }
 
 function startwatch() {
-	watch('app/sass/**/*.sass', { usePolling: true }, styles)
-	watch(['app/**/*.js', 'app/js/common.js'], { usePolling: true }, series(commonJs, scripts))
-	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', sync.reload)
+  watch('app/sass/**/*.sass', styles)
+  watch(['app/**/*.js', 'app/js/common.js'], series(commonJs, scripts))
+  watch(`app/**/*.{${fileswatch}}`).on('change', sync.reload)
 }
+
 function removedist() {
-  return del('dist', { force: true })
+  return del('dist', {
+    force: true
+  })
 }
 
 function distHtml() {
   return src([
     'app/*.html',
-    ]).pipe(dest('dist'))
+  ]).pipe(dest('dist'))
 }
+
 function distStyle() {
   return src([
-		'app/css/main.min.css',
-		]).pipe(dest('dist/css'));
+    'app/css/main.min.css',
+  ]).pipe(dest('dist/css'));
 }
+
 function distScripts() {
-    return src([
-      'app/js/scripts.min.js',
-      ]).pipe(dest('dist/js'));
+  return src([
+    'app/js/scripts.min.js',
+  ]).pipe(dest('dist/js'));
 }
+
 function distPictures() {
-    return src([
-      'app/img/**/*',
-      ]).pipe(dest('dist/img'));
+  return src([
+    'app/img/**/*',
+  ]).pipe(dest('dist/img'));
 }
 
 function distFonts() {
-    return src([
-		'app/fonts/**/*',
-		]).pipe(dest('dist/fonts'));
-    
+  return src([
+    'app/fonts/**/*',
+  ]).pipe(dest('dist/fonts'));
+
 }
 
 // run server
